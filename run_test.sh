bea4d7a1440caf525d6673c635593b54290110ac
dzobbe <giovanni.mazzeo@uniparthenope.it>
2022-10-18 07:09:18 +0200

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,4 @@
 sudo docker stop kms_test
 sudo docker rm kms_test
-sudo docker build -t xmargininc/kms_test_img ./ -f ./Docker/Dockerfile 
-sudo docker run -d --net=latest_appsnet --privileged -v /dev/isgx:/dev/isgx --name kms_test -v $(pwd):/usr/src/app/ xmargininc/kms_test_img 
+sudo docker build -t trustup/sgx_test_img ./ -f ./Dockerfile 
+sudo docker run -d --net=latest_appsnet --privileged -v /dev/isgx:/dev/isgx --name sgx_test -v $(pwd):/usr/src/app/ trustup/sgx_test_img 
